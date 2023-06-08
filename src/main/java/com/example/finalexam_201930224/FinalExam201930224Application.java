1ecf03209eb403e4e71a7a33dc60d46024970669
LSY <112356856+OWL-AT-Midnight@users.noreply.github.com>
2023-06-08 14:18:33 +0900

    Initial commit

---
+++ src/main/java/com/example/finalexam_201930224/FinalExam201930224Application.java
@@ -0,0 +1,13 @@
+package com.example.finalexam_201930224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExam201930224Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExam201930224Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalexam_201930224/FinalExam201930224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalexam_201930224;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExam201930224ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
